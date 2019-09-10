96d581daa98d57aff95f7f42ac25ef362f8b8068
minfanchao <minfanchao@pashanhoo.com>
2019-09-10 19:22:06 +0800

    更新项目

---
+++ service-zuul/src/main/java/com/example/cloud/ServiceZuulApplication.java
@@ -0,0 +1,22 @@
+package com.example.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author minfanchao
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ServiceZuulApplication.class, args );
+    }
+}
+
