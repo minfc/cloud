1c6f8b9783364b20565a6628398bf21ef6c7bb8b
minfanchao <minfanchao@pashanhoo.com>
2019-09-10 18:57:05 +0800

    更新项目

--- service-feign/src/main/java/com/example/cloud/HiController.java
+++ service-feign/src/main/java/com/example/cloud/HiController.java
@@ -1,16 +1,13 @@
 package com.example.cloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HiController {
     @Autowired
     SchedualServiceHi schedualServiceHi;
-    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    @GetMapping(value = "/hi")
     public String sayHi(@RequestParam String name){
         return schedualServiceHi.sayHiFromClientOne(name);
     }
--- service-feign/src/main/java/com/example/cloud/SchedualServiceHi.java
+++ service-feign/src/main/java/com/example/cloud/SchedualServiceHi.java
@@ -6,12 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * Created by fangzhipeng on 2017/4/6.
- */
-@FeignClient(value = "eurka-client")
+@FeignClient(value = "eurka-client", fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
-    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
 }
 
