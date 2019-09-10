cad367259df839ddc8bf962354ece5a47c288ff7
minfanchao <minfanchao@pashanhoo.com>
2019-09-10 17:52:44 +0800

    更新项目

--- service-feign/src/main/java/com/example/cloud/SchedualServiceHi.java
+++ service-feign/src/main/java/com/example/cloud/SchedualServiceHi.java
@@ -1,7 +1,7 @@
 package com.example.cloud;
 
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by fangzhipeng on 2017/4/6.
  */
-@FeignClient(value = "service-hi")
+@FeignClient(value = "eurka-client")
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
--- service-feign/src/main/java/com/example/cloud/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/example/cloud/ServiceFeignApplication.java
@@ -1,25 +1,26 @@
 package com.example.cloud;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author minfanchao
  */
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableFeignClients
-@RestController
 public class ServiceFeignApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ServiceFeignApplication.class, args);
+        SpringApplication.run( ServiceFeignApplication.class, args );
     }
 }
+
+
+
+
