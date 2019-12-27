e1031fc1d210ca7c4f65e34a6c968c28544009cd
minfanchao <minfanchao@pashanhoo.com>
2019-12-27 13:34:00 +0800

    Default Changelist

--- service-feign/src/main/java/com/example/cloud/HiController.java
+++ service-feign/src/main/java/com/example/cloud/HiController.java
@@ -3,9 +3,11 @@ package com.example.cloud;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+
 @RestController
 public class HiController {
-    @Autowired
+    @Resource
     SchedualServiceHi schedualServiceHi;
     @GetMapping(value = "/hi")
     public String sayHi(@RequestParam String name){
