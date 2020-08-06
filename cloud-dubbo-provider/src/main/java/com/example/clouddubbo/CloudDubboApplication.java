5886b3d39e52ec3e99aa0f27ac083ab74297c05f
minfc <minfc@chinaunicom.cn>
2020-08-06 14:33:11 +0800

    1

---
+++ cloud-dubbo-provider/src/main/java/com/example/clouddubbo/CloudDubboApplication.java
@@ -0,0 +1,13 @@
+package com.example.clouddubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDubboApplication.class, args);
+    }
+
+}
